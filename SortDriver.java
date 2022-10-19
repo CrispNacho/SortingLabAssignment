53c7f7362829136cdee5270e3ad95fb404cf5e6c
Evan Chadwick <evanchadwick@hotmail.com>
2022-10-19 14:48:20 -0400

    adding my methods

--- SortDriver.java
+++ SortDriver.java
@@ -1,9 +1,13 @@
 import java.util.Scanner;
 
+
 public class SortDriver{
     /* Java program for Merge Sort */
 	// this is a test
-
+	
+ 
+    
+    
 	void merge(int arr[], int l, int m, int r)
 	{
 		// Find sizes of two subarrays to be merged
@@ -86,6 +90,43 @@ public class SortDriver{
 			System.out.print(arr[i] + " ");
 		System.out.println();
 	}
+	public static int getSortType(){
+        while(true){
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Select a sorting algorithm from the following: 1:Selection Sort 2:Insertion Sort 3:Merge/Heap Sort 4:Quick Sort");
+        String sortType = reader.nextLine();
+        int x = 0;
+        try{x = Integer.parseInt(sortType);
+
+        }catch(Exception numberformatException){
+            System.out.println("Enter number");
+            continue;
+        }
+        if(x < 5 && x > 0){        
+        return x;
+        }
+        System.out.println("Enter number within range");
+        }
+        
+    }
+    public static int getNumOfIntValues(){
+        while(true){
+            Scanner reader = new Scanner(System.in);
+            System.out.println("How many integer values would you like to sort");
+            String numOfIntegers = reader.nextLine();
+            int x = 0;
+            try{x = Integer.parseInt(numOfIntegers);
+    
+            }catch(Exception numberformatException){
+                System.out.println("Enter number");
+                continue;
+            }
+            if(x > 0){        
+            return x;
+            }
+            System.out.println("Enter number above 0");
+            }
+    }
 
 	// Driver method
 	public static void main(String args[])
@@ -100,5 +141,8 @@ public class SortDriver{
 
 		System.out.println("\nSorted array");
 		printArray(arr);
+		//evan stuff
+		int sortType = getSortType();
+        int numOfIntValues = getNumOfIntValues();
 	}
 }
