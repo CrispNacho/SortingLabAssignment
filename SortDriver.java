8afffad5a190d137d5c4d29377359c75c0e6c5cc
Evan Chadwick <evanchadwick@hotmail.com>
2022-10-20 14:34:06 -0400

    adding quick sort

--- SortDriver.java
+++ SortDriver.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 import java.util.Random;
-
+import java.io.*;
 
 public class SortDriver {
 	// Evan
@@ -84,14 +84,92 @@ public class SortDriver {
 		}
 		return arr;
 	}
+    static void swap(int[] arr, int i, int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+  
+    /* This function takes last element as pivot, places
+       the pivot element at its correct position in sorted
+       array, and places all smaller (smaller than pivot)
+       to left of pivot and all greater elements to right
+       of pivot */
+    static int partition(int[] arr, int low, int high)
+    {
+  
+        // pivot
+        int pivot = arr[high];
+  
+        // Index of smaller element and
+        // indicates the right position
+        // of pivot found so far
+        int i = (low - 1);
+  
+        for (int j = low; j <= high - 1; j++) {
+  
+            // If current element is smaller
+            // than the pivot
+            if (arr[j] < pivot) {
+  
+                // Increment index of
+                // smaller element
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return (i + 1);
+    }
+  
+    /* The main function that implements QuickSort
+              arr[] --> Array to be sorted,
+              low --> Starting index,
+              high --> Ending index
+     */
+    static void quickSort(int[] arr, int low, int high)
+    {
+        if (low < high) {
+  
+            // pi is partitioning index, arr[p]
+            // is now at right place
+            int pi = partition(arr, low, high);
+  
+            // Separately sort elements before
+            // partition and after partition
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+  
+    // Function to print an array
+    static void printArray(int[] arr, int size)
+    {
+        for (int i = 0; i < size; i++)
+            System.out.print(arr[i] + " ");
+  
+        System.out.println();
+    }
 	// Driver method
 	public static void main(String args[]) {
 		int sortType = getSortType();
 		int numOfIntValues = getNumOfIntValues();
         if(numOfIntValues == -1){
-            numOfIntValues = getValueFromSet();
+            numOfIntValues = getValueFromSet(); 
         }
-        System.out.println(numOfIntValues);
-		int[] randomNumbers = randomValues(numOfIntValues);
+        int[] randomNumbers = randomValues(numOfIntValues);
+        if(sortType == 1){
+
+        }else if(sortType == 2){
+
+        }else if(sortType == 3){
+            
+        }else if(sortType == 4){
+        quickSort(randomNumbers, 0, numOfIntValues - 1);
+        System.out.println("Sorted array: ");  
+        }
+        printArray(randomNumbers, numOfIntValues);
+        
 	}
 }
