b07ed44018a4dcaeffc82504d7f846df8aeac8e0
kxrai <alicia.2005.loi@gmail.com>
2022-10-21 16:03:45 -0400

    Added Merge Sort

--- SortDriver.java
+++ SortDriver.java
@@ -3,11 +3,11 @@ import java.util.Random;
 import java.io.*;
 
 public class SortDriver {
-	// Evan
+
 	public static int getSortType(){
         while(true){
         Scanner reader = new Scanner(System.in);
-        System.out.println("Select a sorting algorithm from the following: 1:Selection Sort 2:Insertion Sort 3:Merge/Heap Sort 4:Quick Sort");
+        System.out.println("Select a sorting algorithm from the following: 1: Selection Sort 2: Merge/Heap Sort 3: Insertion Sort 4: Quick Sort");
         String sortType = reader.nextLine();
         int x = 0;
         try{x = Integer.parseInt(sortType);
@@ -68,8 +68,7 @@ public class SortDriver {
         }
         
     }
-	
-	//--------------------------------------------------------------------------//
+
 	/**
 	 * Creates an array a certain length depend on parameter
 	 * fills the array with random numbers from 0 to 2147483647
@@ -85,6 +84,81 @@ public class SortDriver {
 		return arr;
 	}
     //Quick sort methods
+
+    void mergeSort(int arr[], int l, int m, int r)
+	{
+		// Find sizes of two subarrays to be merged
+		int n1 = m - l + 1;
+		int n2 = r - m;
+
+		/* Create temp arrays */
+		int L[] = new int [n1];
+		int R[] = new int [n2];
+
+		/*Copy data to temp arrays*/
+		for (int i=0; i<n1; ++i)
+			L[i] = arr[l + i];
+		for (int j=0; j<n2; ++j)
+			R[j] = arr[m + 1+ j];
+
+
+		/* Merge the temp arrays */
+
+		// Initial indexes of first and second subarrays
+		int i = 0, j = 0;
+
+		// Initial index of merged subarray array
+		int k = l;
+		while (i < n1 && j < n2)
+		{
+			if (L[i] <= R[j])
+			{
+				arr[k] = L[i];
+				i++;
+			}
+			else
+			{
+				arr[k] = R[j];
+				j++;
+			}
+			k++;
+		}
+
+		/* Copy remaining elements of L[] if any */
+		while (i < n1)
+		{
+			arr[k] = L[i];
+			i++;
+			k++;
+		}
+
+		/* Copy remaining elements of R[] if any */
+		while (j < n2)
+		{
+			arr[k] = R[j];
+			j++;
+			k++;
+		}
+	}
+
+    // Main function that sorts arr[l..r] using
+	// merge()
+	void sort(int arr[], int l, int r)
+	{
+		if (l < r)
+		{
+			// Find the middle point
+			int m = (l+r)/2;
+
+			// Sort first and second halves
+			sort(arr, l, m);
+			sort(arr , m+1, r);
+
+			// Merge the sorted halves
+			mergeSort(arr, l, m, r);
+		}
+	}
+
     static void quickSortswap(int[] arr, int i, int j)
     {
         int temp = arr[i];
@@ -182,13 +256,15 @@ public class SortDriver {
         if(sortType == 1){
 
         }else if(sortType == 2){
+            MergeSort ob = new MergeSort();
+			ob.sort(randomNumbers, 0, numOfIntValues - 1); //user input array goes here instead
 
         }else if(sortType == 3){
-        SortDriver ob = new SortDriver();
-        ob.insertionSort(randomNumbers);
+            SortDriver ob = new SortDriver();
+            ob.insertionSort(randomNumbers);
         }else if(sortType == 4){
-        quickSort(randomNumbers, 0, numOfIntValues - 1);
-        System.out.println("Sorted array: ");  
+            quickSort(randomNumbers, 0, numOfIntValues - 1);
+            System.out.println("Sorted array: ");  
         }
         long postSort = System.currentTimeMillis();
         System.out.println(postSort - preSort);
