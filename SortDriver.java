08cc24c4f65858c8678735d5579926b0cf69b8b5
kxrai <alicia.2005.loi@gmail.com>
2022-10-22 21:15:24 -0400

    sorting works i think

--- SortDriver.java
+++ SortDriver.java
@@ -85,6 +85,25 @@ public class SortDriver {
 	}
     //Quick sort methods
 
+    void selecionSort(int arr[])
+	{
+		int n = arr.length;
+		for (int i = 0; i < n-1; i++)
+		{
+			// Find the minimum element in unsorted array
+			int min_idx = i;
+			for (int j = i+1; j < n; j++)
+				if (arr[j] < arr[min_idx])
+					min_idx = j;
+
+			// Swap the found minimum element with the first
+			// element
+			int temp = arr[min_idx];
+			arr[min_idx] = arr[i];
+			arr[i] = temp;
+		}
+	}
+    
     void mergeSort(int arr[], int l, int m, int r)
 	{
 		// Find sizes of two subarrays to be merged
@@ -254,6 +273,8 @@ public class SortDriver {
         int[] randomNumbers = randomValues(numOfIntValues);
         long preSort = System.currentTimeMillis();
         if(sortType == 1){
+            SelectionSort ob = new SelectionSort();
+		    ob.selecionSort(randomNumbers);
 
         }else if(sortType == 2){
             SortDriver ob = new SortDriver();
